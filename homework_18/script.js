d7827de47504358a8d5b606aec22ff5fdcf76951
AlexKravchuk1991 <alex_krava@ukr.net>
2024-10-14 17:23:53 +0200

    HM18

---
+++ homework_18/script.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('#button-info')
+const picture = document.querySelector('#img-picture')
+const catFact = document.querySelector('#intesting-info')
+
+
+async function getInterestingInfo() {
+    const res = await fetch('https://catfact.ninja/fact')
+    
+    const data = await res.json();
+
+    const photoRes = await fetch('https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1')
+    const photoData = await photoRes.json();
+
+    picture.src = photoData[0].url;
+    catFact.textContent = data.fact;
+    
+}
+
+button.addEventListener('click',getInterestingInfo);
+getInterestingInfo()
